b38096fbf226a64c4b40d3ab2878e29676a53e22
Zelferion <zelferionedits@gmail.com>
2024-05-10 02:36:05 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center pr-10 pl-10 bg-zinc-950">
       <div className="text-center">
         <h1 className="text-6xl font-bold text-zinc-100">
-          Hello, I'm Zelferion
+          Hello, I&apos;m Zelferion
         </h1>
         <p className="text-2xl font-bold text-zinc-400 m-3">
           and I <span className="text-blue-700">suck</span> at coding
@@ -13,8 +13,8 @@ export default function Home() {
       </div>
       <div className="w-full absolute bottom-0 px-12 pb-12">
         <p className="text-3xl text-zinc-100 text-center">
-          I'm using <span className="text-blue-700 font-bold">Next.js</span>{" "}
-          because
+          I&apos;m using{" "}
+          <span className="text-blue-700 font-bold">Next.js</span> because
           <span className="text-green-800 font-bold"> Vue.js </span> does not
           work for me lol
         </p>
